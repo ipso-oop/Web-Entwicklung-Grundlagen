58399acd866248c4dcffc52dd72395e2f7a18a5c
Roman <roman.camenzind@dozent.ipso.ch>
2025-01-10 14:28:54 +0100

    Beispiel Statische Webseite hinzugefügt

---
+++ Grundlagen/Beispiele/dynamic/scripts.js
@@ -0,0 +1,4 @@
+function changeText() {
+    const introParagraph = document.getElementById('intro');
+    introParagraph.textContent = "Ich bin ein sehr erfahrener Architekt und Software-Entwickler ich schaue auf über 20 Jahre Erfahrung in diversen Technologien und Branchen zurück.";
+}
